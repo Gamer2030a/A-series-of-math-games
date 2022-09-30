823bd29795ebc5ab2f1cb3d495d3b85b07c4a531
ShayanDJ65git config --global user.name ShayanDJ65git config --global user.name ShayanDJ65 <shayanhosseinzadeh1@gmail.com>
2022-09-30 21:38:33 +0330

    Fixed Button's Cordinates.

--- SRC/Application.py
+++ SRC/Application.py
@@ -7,46 +7,23 @@ ct.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 # Windows 
 root = ct.CTk()  # create CTk window like you do with the Tk window
-Gamepage = tk.Frame(root)  # Game Page
-Startpage = tk.Frame(root) # Start Page
 
-# Copied 
+# new game buttn function
 def change_to_newgame():
-    newgame_btn.pack_forget()
-    Continue_btn.grid_forget()
-    Settings_btn.pack_forget()
-    Exit_btn.pack_forget()
-    
-    
-    
-
+ btn_frame.place_forget() 
 # Dimension, Icon and Title
 root.title("GamesName")
 # root.iconbitmap("SRC/rootlication/STTEST.ico")
 root.geometry("1280x720")
+
 # Buttons 
-def button_NewGame():
-    print("button NewGame")
-newgame_btn = ct.CTkButton(master=root,width=220, text="NewGame",corner_radius=3,command=change_to_newgame)
-# button.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
-newgame_btn.pack()
-
-def button_Continue():
-    print("button Continue")
-Continue_btn = ct.CTkButton(master=root,width=220, text="Repository",corner_radius=3, command=button_Continue)
-Continue_btn.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
-
-def button_Settings():
-    print("button Settings")
-Settings_btn = ct.CTkButton(master=root,width=220, text="Settings",corner_radius=3, command=button_Settings)
-Settings_btn.place(relx=0.5, rely=0.85, anchor=tk.CENTER)
-
-def button_Exit():
-    print("button Exit")
-    quit()
-Exit_btn = ct.CTkButton(master=root,width=220, text="Exit",corner_radius=3, command=button_Exit)
-Exit_btn.place(relx=0.5, rely=0.9, anchor=tk.CENTER)
-
+btn_frame = tk.Frame(root,bg="#222325") 
+Newgame_btn = ct.CTkButton(btn_frame, text = "New Game",corner_radius=3, command=change_to_newgame).grid(row = 0, column = 1, pady=3)
+Continue_btn = ct.CTkButton(btn_frame, text = "Continue",corner_radius=3).grid(row = 1, column = 1, pady=3)
+Repository_btn = ct.CTkButton(btn_frame, text = "Repository",corner_radius=3).grid(row = 2, column = 1, pady=3)
+exit_btn = ct.CTkButton(btn_frame, text = "Exit",corner_radius=3).grid(row = 3, column = 1, pady=3)
+btn_frame.place(relx = 0.45, rely=0.75)
+    
 # Copyright
 copyright = tk.StringVar(value="Game made by Parsa dehghani & Shayan Hosseinzadeh")
 label = ct.CTkLabel(master=root,
