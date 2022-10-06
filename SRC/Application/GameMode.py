b9c499dfd8c3ed53b7c898a05dcc4c09989fc8c5
Parsa <Gamer2030fasterthangame@gmail.com>
2022-10-06 14:10:39 +0330

    game mode updated for difficultly levels

--- SRC/Application/GameMode.py
+++ SRC/Application/GameMode.py
@@ -10,6 +10,30 @@ def PlayBGMusic():
     mixer.init()
     mixer.music.load(r'SRC\Assets\SFX\bgmusic.mp3')
     mixer.music.play()
+#============== Difficulty  ===============
+# 0 = easy
+# 1 = medium
+# 2 = hard
+# 3 = nightmare
+
+def GiveQuestion():
+    global GameDifficulty
+    
+    if GameDifficulty == 0:
+        print("Easy Question")
+        return 0
+
+    elif GameDifficulty == 1:
+        print("Medium Question")
+        return 1
+
+    elif GameDifficulty == 2:
+        print("Hard Question")
+        return 2
+
+    else:
+        print("Nightmare Question")
+        return 3
 #===========================================
 
      
